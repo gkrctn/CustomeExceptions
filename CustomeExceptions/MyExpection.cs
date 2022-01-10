48fe81e8072f89cafbcc0cca06cd1faf2796db5f
gkrctn <gkr.cetin@gmail.com>
2022-01-10 20:49:30 +0300

    BenimHatam

---
+++ CustomeExceptions/MyExpection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomeExceptions
+{
+    public class MyCustomExpection : Exception
+    {
+        string isim;
+
+        public override string Message
+        {
+            get
+            {
+                return "Girdiğiniz isim : " + isim + "....A ile başlayan isim girmeyiniz";
+            }
+        }
+        public override string HelpLink
+        {
+            get
+            {
+                return "www.google.com";
+            }
+            set
+            {
+                base.HelpLink = value;
+            }
+        }
+        public MyCustomExpection(string isim)
+        {
+            this.isim = isim;
+        }
+            
+
+
+
+    }
+}
--- CustomeExceptions/Program.cs
+++ CustomeExceptions/Program.cs
@@ -4,73 +4,75 @@ namespace CustomeExceptions
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            int sayi = 0;
-            int sayac;
+        //static void Main(string[] args)
+        //{
+        //    int sayi = 0;
+        //    int sayac;
 
-            do
-            {
-                sayac = 0;
-                Console.WriteLine("Lütfen bir sayı giriniz : ");
-                try
-                {
+        //    do
+        //    {
+        //        sayac = 0;
+        //        Console.WriteLine("Lütfen bir sayı giriniz : ");
+        //        try
+        //        {
                    
 
-                    sayi = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Girilen sayı +5 : " + (sayi + 5));
-                    Console.WriteLine("Devam..");
+        //            sayi = int.Parse(Console.ReadLine());
+        //            Console.WriteLine("Girilen sayı +5 : " + (sayi + 5));
+        //            Console.WriteLine("Devam..");
 
-                    int bolen = 0;
-                    try
-                    {
-                        Console.WriteLine("Lütfen böleni giriniz");
-                        bolen = int.Parse(Console.ReadLine());
-                    }
-                    catch (Exception)
-                    {
-                        throw;
-                    }
-                    Console.WriteLine(sayi/bolen);
-                }
-                catch(DivideByZeroException dbze)
-                {
-                    Console.WriteLine((dbze.StackTrace);
-                    Console.WriteLine("Tanımsız");
-                    sayac++;
-                }
-                catch (ArgumentException)
-                {
+        //            int bolen = 0;
+        //            try
+        //            {
+        //                Console.WriteLine("Lütfen böleni giriniz");
+        //                bolen = int.Parse(Console.ReadLine());
+        //            }
+        //            catch (Exception)
+        //            {
+        //                throw;
+        //            }
+        //            Console.WriteLine(sayi/bolen);
+        //        }
+        //        catch(DivideByZeroException dbze)
+        //        {
+        //            Console.WriteLine((dbze.StackTrace);
+        //            Console.WriteLine("Tanımsız");
+        //            sayac++;
+        //        }
+        //        catch (ArgumentException)
+        //        {
 
-                    Console.WriteLine("Değer boş olamaz");
-                }
-                catch (FormatException ex)
-                {
+        //            Console.WriteLine("Değer boş olamaz");
+        //        }
+        //        catch (FormatException ex)
+        //        {
 
-                    Console.WriteLine("Lütfen doğru formatta değer giriniz..");
-                    //Console.WriteLine(ex.Message);
-                    //Console.WriteLine(ex.Source);
-                    //Console.WriteLine(ex.HelpLink);
-
-
-                }
-                catch (OverflowException)
-                {
-
-                    Console.WriteLine("Değer 2,147,483,647 büyük olamaz");
-                }
-                catch (Exception)
-                {
-
-                    throw;
-                }
+        //            Console.WriteLine("Lütfen doğru formatta değer giriniz..");
+        //            //Console.WriteLine(ex.Message);
+        //            //Console.WriteLine(ex.Source);
+        //            //Console.WriteLine(ex.HelpLink);
 
 
-            } while (true);
+        //        }
+        //        catch (OverflowException)
+        //        {
+
+        //            Console.WriteLine("Değer 2,147,483,647 büyük olamaz");
+        //        }
+        //        catch (Exception)
+        //        {
+
+        //            throw;
+        //        }
+
+
+        //    } while (true);
+
+        //}
 
 
 
-            
-        }
+
+
     }
 }
